0c9c6a002a243cf9125ae31712fc27e1db5482dd
faheem <faheemka699@gmail.com>
2025-08-23 00:36:42 +0530

    Initial commit of my portfolio

---
+++ index.js
@@ -0,0 +1,100 @@
+// document.getElementsByClassName("new").innerHTML="JAVASRIPT"
+
+
+//Type conversion
+// let age= window.prompt("How Old Are You")
+// age= Number(age);
+// age+=1;
+// console.log(age,typeof age);
+
+// let x ="pizza";
+// let y ="pizza";
+// let z ="pizza";
+
+// x= Number(x);
+// y=String(y);
+// z=Boolean(z );
+
+// console.log(x,typeof x);
+// console.log(y,typeof y);
+// console.log(z,typeof z);
+
+
+// console.log('I am' + 23 + "old");
+
+
+// conditional statement:
+// if else
+// let name = 'Mango'
+
+// if(name == 'Butterfly'){
+//     alert('There is a Butterfly')
+// }
+// else{
+//     alert("Its not Mango its Butterfly")
+// }
+
+// Ternary operator
+// let age= 18
+// let message = (age > 17)?"You are an adult":"You are minor";
+// console.log(message);
+
+// for(let i = 0;i<5;i++){
+//     console.log(i);
+// }
+// while loop
+// let count = 0;
+// while(count<5){
+//     console.log(count);
+//     count++;
+// }
+
+// do-while loop
+// let count=7
+// do{
+//     console.log(count);
+//     count++;
+// }
+// while(count<6);
+
+
+// for(let i = 1; i <= 4; i++) {
+//     let row = "";
+//     for(let j = 0; j < i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+// 
+// *
+// **
+// ***
+// ****}
+
+
+// for(let i=5;i >=1; i--){
+//     let row="";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+        
+//     }
+//     console.log(row);
+// }
+
+// for(let i=4;i >=4; i++){
+//     let row="";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+        
+//     }
+//     console.log(row);
+// }
+
+let phone = {
+    brand:"samsung",
+    cpu:"snapdragon",
+    ram:8,
+    Storage:"128gb" 
+}
+
+console.log(phone);
---
+++ script.js
@@ -0,0 +1,53 @@
+// Bubble effect
+// const bubble =document.querySelector('.bubble');
+// if(bubble){
+//     document.addEventListener('mousemove',(e) =>{
+//         bubble.style.transform = `translate(${e.clientX}px,${e.clientY}px)`
+//     });
+// }
+// Bubble follows the mouse
+// const bubble = document.querySelector('.bubble');
+
+// if (bubble) {
+//   document.addEventListener('mousemove', (e) => {
+//     bubble.style.transform = `translate(${e.clientX - 150}px, ${e.clientY - 150}px)`; 
+//   });
+// }
+
+
+// Simple form handling (can replace with EmailJS later)
+function sendEmail(event) {
+  event.preventDefault();
+
+  let params = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+  };
+
+  const serviceID = "service_32tj4d8";
+  const templateID = "template_urdsjmj";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+      console.log(res);
+      alert("Your message has been sent successfully!");
+    })
+    .catch(err => console.log(err));
+}
+
+//text animation like hacking
+const text="Hi, I'm Faheem Shan👋";
+const animatedText = document.getElementById("animatedText");
+let i =0;
+function typeEffect(){
+  if (i < text.length) {
+        animatedText.textContent += text.charAt(i); // add one letter
+        i++;
+        setTimeout(typeEffect, 100); // wait 100ms, then call again
+      }
+    }
+  window.onload = typeEffect; // run function when page loads
